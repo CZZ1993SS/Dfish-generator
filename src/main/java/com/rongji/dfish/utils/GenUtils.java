647ec853ba1a85d20341dd3f6ce7d852311e14cd
czz1993s <1163390389@163.com>
2021-11-29 18:33:39 +0800

    1）调整

--- src/main/java/com/rongji/dfish/utils/GenUtils.java
+++ src/main/java/com/rongji/dfish/utils/GenUtils.java
@@ -1,7 +1,18 @@
 package com.rongji.dfish.utils;
 
-import com.rongji.dfish.entity.ColumnEntity;
-import com.rongji.dfish.entity.TableEntity;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -12,12 +23,8 @@ import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.*;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import com.rongji.dfish.entity.ColumnEntity;
+import com.rongji.dfish.entity.TableEntity;
 
 public class GenUtils {
 
@@ -25,8 +32,8 @@ public class GenUtils {
     private static List<String> getTemplates() {
         List<String> templates = new ArrayList<>();
         templates.add("template/Entity.java.vm");
-        templates.add("template/Dao.java.vm");
-        templates.add("template/Service.java.vm");
+        //templates.add("template/Dao.java.vm");
+        //templates.add("template/Service.java.vm");
         templates.add("template/Controller.java.vm");
         return templates;
     }
@@ -49,19 +56,19 @@ public class GenUtils {
      */
     public static void generatorCode(Map<String, String> table,
                                      List<Map<String, String>> columns, ZipOutputStream zip) {
-        //配置信息
+        // 配置信息
         Configuration config = getConfig();
         boolean hasBigDecimal = false;
-        //表信息
+        // 表信息
         TableEntity tableEntity = new TableEntity();
         tableEntity.setTableName(table.get("tableName"));
         tableEntity.setComments(table.get("tableComment"));
-        //表名转换成Java类名
+        // 表名转换成Java类名
         String className = tableToJava(tableEntity.getTableName(), config.getStringArray("tablePrefix"));
         tableEntity.setClassName(className);
         tableEntity.setClassname(StringUtils.uncapitalize(className));
 
-        //列信息
+        // 列信息
         List<ColumnEntity> columsList = new ArrayList<>();
         for (Map<String, String> column : columns) {
             ColumnEntity columnEntity = new ColumnEntity();
@@ -70,18 +77,18 @@ public class GenUtils {
             columnEntity.setComments(column.get("columnComment"));
             columnEntity.setExtra(column.get("extra"));
 
-            //列名转换成Java属性名
+            // 列名转换成Java属性名
             String attrName = columnToJava(columnEntity.getColumnName());
             columnEntity.setAttrName(attrName);
             columnEntity.setAttrname(StringUtils.uncapitalize(attrName));
 
-            //列的数据类型，转换成Java类型
+            // 列的数据类型，转换成Java类型
             String attrType = config.getString(columnEntity.getDataType(), "unknowType");
             columnEntity.setAttrType(attrType);
             if (!hasBigDecimal && attrType.equals("BigDecimal")) {
                 hasBigDecimal = true;
             }
-            //是否主键
+            // 是否主键
             if ("PRI".equalsIgnoreCase(column.get("columnKey")) && tableEntity.getPk() == null) {
                 tableEntity.setPk(columnEntity);
             }
@@ -90,18 +97,18 @@ public class GenUtils {
         }
         tableEntity.setColumns(columsList);
 
-        //没主键，则第一个字段为主键
+        // 没主键，则第一个字段为主键
         if (tableEntity.getPk() == null) {
             tableEntity.setPk(tableEntity.getColumns().get(0));
         }
 
-        //设置velocity资源加载器
+        // 设置velocity资源加载器
         Properties prop = new Properties();
         prop.put("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
         Velocity.init(prop);
         String mainPath = config.getString("mainPath");
         mainPath = StringUtils.isBlank(mainPath) ? "com.rongji" : mainPath;
-        //封装模板数据
+        // 封装模板数据
         Map<String, Object> map = new HashMap<>();
         map.put("tableName", tableEntity.getTableName());
         map.put("comments", tableEntity.getComments());
@@ -119,10 +126,10 @@ public class GenUtils {
         map.put("datetime", DateUtils.format(new Date(), DateUtils.DATE_TIME_PATTERN));
         VelocityContext context = new VelocityContext(map);
 
-        //获取模板列表
+        // 获取模板列表
         List<String> templates = getTemplates();
         for (String template : templates) {
-            //渲染模板
+            // 渲染模板
             StringWriter sw = new StringWriter();
             Template tpl = Velocity.getTemplate(template, "UTF-8");
             tpl.merge(context, sw);
@@ -132,7 +139,7 @@ public class GenUtils {
                 /*zip.putNextEntry(new ZipEntry(getFileName(template, tableEntity.getClassName(),
                   config.getString("package" ), config.getString("moduleName" ))));*/
 
-                //添加到zip
+                // 添加到zip
                 zip.putNextEntry(new ZipEntry(Objects.requireNonNull(getFileName(template, tableEntity.getClassName(),
                   config.getString("package"), config.getString("moduleName")))));
 
@@ -178,30 +185,30 @@ public class GenUtils {
         }
 
         if (template.contains("Entity.java.vm")) {
-            return packagePath + "entity" + File.separator + className + "Entity.java";
+            return packagePath + "entity" + File.separator + className + ".java";
         }
-
-        if (template.contains("Dao.java.vm")) {
-            return packagePath + "dao" + File.separator + className + "Dao.java";
-        }
-
-        if (template.contains("Service.java.vm")) {
-            return packagePath + "service" + File.separator + className + "Service.java";
-        }
+        //
+        // if (template.contains("Dao.java.vm")) {
+        //     return packagePath + "dao" + File.separator + className + "Dao.java";
+        // }
+        //
+        // if (template.contains("Service.java.vm")) {
+        //     return packagePath + "service" + File.separator + className + "Service.java";
+        // }
 
         if (template.contains("Controller.java.vm")) {
             return packagePath + "controller" + File.separator + className + "Controller.java";
         }
 
-        if (template.contains("index.vue.vm")) {
-            return "main" + File.separator + "resources" + File.separator + "src" + File.separator + "views" + File.separator + "modules" +
-              File.separator + moduleName + File.separator + className.toLowerCase() + ".vue";
-        }
-
-        if (template.contains("add-or-update.vue.vm")) {
-            return "main" + File.separator + "resources" + File.separator + "src" + File.separator + "views" + File.separator + "modules" +
-              File.separator + moduleName + File.separator + className.toLowerCase() + "-add-or-update.vue";
-        }
+        // if (template.contains("index.vue.vm")) {
+        //     return "main" + File.separator + "resources" + File.separator + "src" + File.separator + "views" + File.separator + "modules" +
+        //       File.separator + moduleName + File.separator + className.toLowerCase() + ".vue";
+        // }
+        //
+        // if (template.contains("add-or-update.vue.vm")) {
+        //     return "main" + File.separator + "resources" + File.separator + "src" + File.separator + "views" + File.separator + "modules" +
+        //       File.separator + moduleName + File.separator + className.toLowerCase() + "-add-or-update.vue";
+        // }
 
         return null;
     }
